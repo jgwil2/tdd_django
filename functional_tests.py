b78d0702197d4f196e23d1d2aa87cc1e121781b4
John Willard <john@Johns-MacBook-Air.local>
2015-01-02 22:54:05 -0500

    first FT specced out in comments and now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,23 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+# set firefox binary to symlink in user directory (cask firefox)
+firefox_binary = FirefoxBinary('/Users/john/Applications/Firefox.app/Contents/MacOS/firefox-bin')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox(firefox_binary=firefox_binary)
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
