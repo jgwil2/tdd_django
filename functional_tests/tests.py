563df5dccfc6957abdb17324081b8cf27d2576ac
John Willard <jgwil2@gmail.com>
2015-03-26 22:35:28 -0400

    functional test to check different urls for different users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -38,6 +38,8 @@ class NewVisitorTest(LiveServerTestCase):
         )
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -45,6 +47,28 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # use new browser session to ensure no information comes
+        # through from cookies, etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox(firefox_binary=firefox_binary)
+        
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
 
         self.fail('Finish the test!')
 
