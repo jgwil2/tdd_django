6ed246fb60a86bfa8966f6a144bdfb79af1f3818
John Willard <jgwil2@gmail.com>
2015-03-29 13:29:37 -0400

    add list model, adjust test and views

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 
@@ -9,7 +9,8 @@ def home_page(request):
     return render(request, 'lists/home.html')
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list/')
 
 def view_list(request):
